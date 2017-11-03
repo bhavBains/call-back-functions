7419c5fd88e11cb54798c55f2c35a15fb2947fc1
bhavBains <deepbains26@gmail.com>
2017-11-03 04:00:25 +0000

    cleaner version--program to roll a dice with pre set array using callbacks

--- cheatDice.js
+++ cheatDice.js
@@ -3,17 +3,13 @@
 
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  //var count = 0;								//Cleaner version
+  								
   
-  var count = -1;
+  var count = -1; //Cleaner version : var count = 0
   return function() {
     for (var i = 0; i<list.length; i++){
-    	//var roll = list[count];   //Cleaner version
-    	
-    	count++;
-    	//return roll;							//Cleaner version
-    	
-    	return list[count]
+    	count++;	//Cleaner version :	var roll = list[count];
+    	return list[count] 	//Cleaner version : return roll;
     }
   }
 }
